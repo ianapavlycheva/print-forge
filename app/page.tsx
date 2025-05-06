7edede575da5284807b94cb48105977037510d3b
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-05-06 18:06:51 +1000

    Set initial project

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Discover what's possible with 3D printing</h1>
+      <p>
+        Join our community of creators and explore a vast library of
+        user-submitted models.
+      </p>
+
+      <button>Browse Models</button>
+      <img src="/home-page-hero-img.png" />
+    </main>
+  );
+}
